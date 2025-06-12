049039df280521778eccb5102a4b4765e013b6e4
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-06-11 22:57:11 -0500

    Error con la colencción [ Jefes ]

--- src/controllers/jefe_controller.js
+++ src/controllers/jefe_controller.js
@@ -11,12 +11,12 @@ const registerBoss = async (req,res)=>{
     if(verificarEmailBDD) 
         return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
     // Registro de nuevo usuario
-    const nuevoAdmin = new Boss(req.body)
+    const nuevoBoss = new Boss(req.body)
     // Encriptación de la contraseña
-    nuevoAdmin.password = await nuevoAdmin.encrypPassword(password)
-    const token = await nuevoAdmin.createToken()
+    nuevoBoss.password = await nuevoBoss.encrypPassword(password)
+    const token = await nuevoBoss.createToken()
     await sendMailToNewBoss(email,token)
-    await nuevoAdmin.save()
+    await nuevoBoss.save()
     res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
 }
 
@@ -33,20 +33,20 @@ const confirmEmail = async (req,res)=>{
 const recoverPassword = async(req,res)=>{
     const {email} = req.body
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    const administradorBDD = await Boss.findOne({email})
-    if(!administradorBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
-    const token = await administradorBDD.createToken()
-    administradorBDD.token=token
+    const BossBDD = await Boss.findOne({email})
+    if(!BossBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
+    const token = await BossBDD.createToken()
+    BossBDD.token=token
     await sendMailToRecoveryPasswordBoss(email,token)
-    await administradorBDD.save()
+    await BossBDD.save()
     res.status(200).json({msg:"Revisa tu correo electrónico para reestablecer tu cuenta"})
 }
 
 const comprobarTokenPasword = async (req,res)=>{
     const token = req.params.token
-    const administradorBDD = await Boss.findOne({token})
-    if(administradorBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
-    await administradorBDD.save()
+    const BossBDD = await Boss.findOne({token})
+    if(BossBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+    await BossBDD.save()
     res.status(200).json({msg:"Token confirmado, ya puedes crear tu nuevo password"}) 
 }
 
@@ -54,11 +54,11 @@ const createNewPassword = async (req,res)=>{
     const{password,confirmpassword} = req.body
     if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
     if(password != confirmpassword) return res.status(404).json({msg:"Lo sentimos, los passwords no coinciden"})
-    const administradorBDD = await Boss.findOne({token:req.params.token})
-    if(administradorBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
-    administradorBDD.token = null
-    administradorBDD.password = await administradorBDD.encrypPassword(password)
-    await administradorBDD.save()
+    const BossBDD = await Boss.findOne({token:req.params.token})
+    if(BossBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+    BossBDD.token = null
+    BossBDD.password = await BossBDD.encrypPassword(password)
+    await BossBDD.save()
     res.status(200).json({msg:"Felicitaciones, ya puedes iniciar sesión con tu nuevo password"}) 
 }
 
