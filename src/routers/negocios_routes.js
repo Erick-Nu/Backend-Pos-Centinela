59c29fceebe3bcd544bcc9001cc29b6497a95497
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-06-13 12:45:05 -0500

    Negocios Rutas

--- src/routers/negocios_routes.js
+++ src/routers/negocios_routes.js
@@ -6,7 +6,7 @@ import {
 
 const router = Router();
 
-router.post('/negocios/create', createNegocio);
-router.post('/negocios/add-employee', addEmployee);
+router.post('/negocio/create', createNegocio);
+router.post('/negocio/add-employee', addEmployee);
 
 export default router;
