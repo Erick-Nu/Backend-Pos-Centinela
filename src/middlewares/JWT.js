275b452fbe211a1f3dd28a23deabc3f6646782ec
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-08-03 21:00:54 -0500

    [ Corrección ]: De JWT.js

--- src/middlewares/JWT.js
+++ src/middlewares/JWT.js
@@ -7,32 +7,30 @@ const createTokenJWT = (id, rol) => {
     return jwt.sign({id, rol}, process.env.JWT_SECRET, {expiresIn: '1d'});
 };
 
-const verifyTokenJWT = async(req, res, next) => {
-    const {authorization} = req.headers;
+const verifyTokenJWT = async (req, res, next) => {
+    const { authorization } = req.headers;
     if (!authorization)
-        return res.status(401).json({msg: "Acceso denegado: Token no proporcionado"});
+        return res.status(401).json({ msg: "Acceso denegado: Token no proporcionado" });
+
     try {
         const token = authorization.split(' ')[1];
-        const {id, rol} = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, rol } = jwt.verify(token, process.env.JWT_SECRET);
+
         if (rol === 'administrador') {
-            req.administradorBDD = await Administrador.findById(id).select('-password -token -__v -isDeleted');
-            next();
+        req.administradorBDD = await Administrador.findById(id).select('-password -token -__v -isDeleted');
+        } else if (rol === 'jefe') {
+        req.jefeDBB = await Boss.findById(id).select('-password -token -__v -isDeleted');
+        } else if (rol === 'empleado') {
+        req.empleadoBDD = await Empleado.findById(id).select('-password -token -__v -isDeleted');
+        } else {
+        return res.status(401).json({ msg: "Acceso denegado: Rol no reconocido" });
         }
-        if (rol === 'jefe') {
-            req.jefeBDD = await Boss.findById(id).select('-password -token -__v -isDeleted');
-            next();
-        }
-        if (rol === 'empleado') {
-            req.empleadoBDD = await Empleado.findById(id).select('-password -token -__v -isDeleted');
-            next();
-        }
-        else {
-            return res.status(401).json({msg: "Acceso denegado: Rol no reconocido"});
-        }
+        next(); 
+
     } catch (error) {
-        return res.status(401).json({msg: "Acceso denegado: Token inválido o expirado"});
+        return res.status(401).json({ msg: "Acceso denegado: Token inválido o expirado" });
     }
-}
+};
 
 export {
     createTokenJWT,
