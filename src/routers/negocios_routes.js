0427ddc0e938caea613e6d0b82b2097d86dfa4c2
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-08-06 20:44:49 -0500

    Verificación del servidor numero 06

--- src/routers/negocios_routes.js
+++ src/routers/negocios_routes.js
@@ -19,7 +19,7 @@ const router = Router();
 // Rutas privadas para negocios
 router.post('/negocios/create', verifyTokenJWT, createNegocio);
 router.get('/negocios/list', verifyTokenJWT, listNegocios);
-router.get('/negocios/list/employees', verifyTokenJWT, listEmployees);
+router.get('/negocios/list/employees/:id', verifyTokenJWT, listEmployees);
 router.post('/negocios/add-employee', verifyTokenJWT, addEmployee); 
 router.get('/negocios/detail/:negocioId', verifyTokenJWT, detalleNegocio);
 router.put('/negocios/update/:negocioId', verifyTokenJWT, updateNegocio);
