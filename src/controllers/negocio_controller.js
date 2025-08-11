e08e64837cc413b49b56433cc2bb15153ce27dcd
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-08-11 06:48:50 -0500

    Correcion del metodo agregar empleado 4

--- src/controllers/negocio_controller.js
+++ src/controllers/negocio_controller.js
@@ -192,7 +192,7 @@ const listEmployees = async (req, res) => {
             .populate({
                 path: "empleados",
                 select: "_id nombres apellidos email foto status",
-                match: { isDeleted: false }
+                match: { isDeleted: true }
             });
         if (!negocioBDD) {
             return res.status(404).json({ msg: "Lo sentimos, no existe el negocio" });
