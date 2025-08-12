aae289b13c8fb85db76f2a0ed28a19b3cc739b23
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-08-12 01:50:16 -0500

    Chat en tiempo real 2

--- src/index.js
+++ src/index.js
@@ -12,15 +12,22 @@ const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
         origin: process.env.URL_FRONTEND,
+        methods: ["GET", "POST"],
     }
 })
 
 io.on('connection', (socket) => {
-    console.log('Usuario conectado',socket.id)
+    console.log(`✅ Usuario conectado con ID: ${socket.id}`)
     socket.on('enviar-mensaje-front-back',(payload)=>{
+        console.log(`📩 Mensaje recibido de ${socket.id}: ${payload}`);
         socket.emit('enviar-mensaje-front-back',payload)
     })
 })
+
+socket.on('disconnect', () => {
+    console.log(`❌ Usuario desconectado: ${socket.id}`);
+});
+
 
 
 server.listen(app.get('port'),()=>{
